5bb872c530e6efda3d0771a41c8822fa589f0f0b
Cikuozzo <roccorotondo@tutamail.com>
2026-01-07 16:38:07 +0100

    updated emacs config and changed repo dir structure

---
+++ Editor/Emacs/.emacs.custom.el
@@ -0,0 +1,21 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(fringe-mode 0 nil (fringe))
+ '(global-display-line-numbers-mode t)
+ '(inhibit-startup-screen t)
+ '(package-selected-packages nil)
+ '(scroll-bar-mode nil)
+ '(size-indication-mode t)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
