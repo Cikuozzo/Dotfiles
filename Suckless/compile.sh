68e8c526516cded28df57b66bad9130ced6e187c
Rocco ROtondo <rocco@PC-Rocco.Home>
2026-02-03 14:17:38 +0100

    added compilation script

---
+++ Suckless/compile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ~/Dotfiles/Suckless/dwm
+sudo make clean install
+cd ..
+cd dmenu
+sudo make clean install
+cd ..
+cd st
+sudo make clean install
+cd ..
+cd slstatus
+sudo make clean install
